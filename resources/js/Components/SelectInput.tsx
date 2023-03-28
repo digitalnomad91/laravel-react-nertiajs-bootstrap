96c3ee8e611ba7b8f0d9089510bd4d7b00637b41
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-28 15:32:27 -0500

    Prettify.

--- resources/js/Components/SelectInput.tsx
+++ resources/js/Components/SelectInput.tsx
@@ -1,29 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-export default ({
-  label,
-  name,
-  className,
-  children,
-  errors = [],
-  ...props
-}) => {
-  return (
-    <div className={className}>
-      {label && (
-        <label className="form-label" htmlFor={name}>
-          {label}:
-        </label>
-      )}
-      <select
-        id={name}
-        name={name}
-        {...props}
-        className={`form-select ${errors.length ? 'error' : ''}`}
-      >
-        {children}
-      </select>
-      {errors && <div className="form-error">{errors}</div>}
-    </div>
-  );
-};
+export default ({ label, name, className, children, errors = [], ...props }) => {
+    return (
+        <div className={className}>
+            {label && (
+                <label className="form-label" htmlFor={name}>
+                    {label}:&nbsp;
+                </label>
+            )}
+            <select id={name} name={name} {...props} className={`form-select ${errors.length ? 'error' : ''}`}>
+                {children}
+            </select>
+            {errors && <div className="form-error">{errors}</div>}
+        </div>
+    )
+}
