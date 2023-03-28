40e1f7faf4f1f4f0a2914904d625bde5be93cb64
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-28 13:03:28 -0500

    Fixed class name.

--- database/migrations/2015_08_04_130520_create_articles_table.php
+++ database/migrations/2015_08_04_130520_create_articles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateSnippetsTable2 extends Migration
+class CreateSnippetsTable extends Migration
 {
     /**
      * Run the migrations.
