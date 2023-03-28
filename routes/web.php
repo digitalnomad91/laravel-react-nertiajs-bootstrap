0a7edb4e831564112b1720322e7168e9976d913b
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-28 13:04:14 -0500

    Updated Payment Routes.

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,7 @@ use App\Http\Controllers\StripePaymentController;
 
 Route::get('/', 'App\Http\Controllers\SnippetsController@index')->name('home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', 'App\Http\Controllers\SnippetsController@index')->name('dashboard');
 });
 
@@ -30,14 +26,12 @@ Route::get('/payment', function () {
     return Inertia::render('Payment');
 })->name('payment');
 
-Route::controller(StripePaymentController::class)->group(function(){
-    Route::get('stripe', 'stripePost')->name('stripe.post');
-    Route::get('success', 'successfulPayment')->name('stripe.success');
+Route::controller(StripePaymentController::class)->group(function () {
+    Route::get('/payment/credit_card', 'stripePost')->name('stripe.post');
+    Route::get('/payment/confirm', 'successfulPayment')->name('stripe.confirm');
 });
 
-
 Route::get('/snippets', 'App\Http\Controllers\Admin\SnippetCrudController@getSnippets')->name('request_snippets');
-
 
 Route::get('/admin/login', function () {
     return redirect('/login');
