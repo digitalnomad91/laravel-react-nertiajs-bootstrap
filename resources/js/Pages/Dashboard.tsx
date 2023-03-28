060276c214f15489d57a40dca04f6d09afe57365
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-28 12:08:50 -0500

    Page title fix.

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -43,7 +43,7 @@ export default function Dashboard() {
 
     return (
         <AppLayout
-            title="Dashboard"
+            title="Code Snippets"
             renderHeader={() => <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
         >
             <div className="py-4">
