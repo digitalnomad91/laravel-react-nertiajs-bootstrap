6eb5535d4d0b6d328fc54ce03f7487194e7ff2ff
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-28 13:04:01 -0500

    Updated title.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'IOPrompts') }}</title>
 
     <meta http-equiv="Content-Security-Policy" 
      content="
