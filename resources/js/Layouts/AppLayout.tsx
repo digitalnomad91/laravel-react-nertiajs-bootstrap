20f3c68293731e3584cc7dc5a2021099d51ddf7f
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-30 14:32:43 -0500

    Added white/dark mode button & other fixes.

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -34,6 +34,47 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
             ;(await import('tw-elements')).default
         }
         use()
+        const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon')
+        const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon')
+
+        // Change the icons inside the button based on previous settings
+        if (
+            localStorage.getItem('color-theme') === 'dark' ||
+            (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+        ) {
+            themeToggleLightIcon?.classList.remove('hidden')
+        } else {
+            themeToggleDarkIcon?.classList.remove('hidden')
+        }
+
+        var themeToggleBtn = document.getElementById('theme-toggle')
+
+        themeToggleBtn?.addEventListener('click', function () {
+            // toggle icons inside button
+            themeToggleDarkIcon?.classList.toggle('hidden')
+            themeToggleLightIcon?.classList.toggle('hidden')
+
+            // if set via local storage previously
+            if (localStorage.getItem('color-theme')) {
+                if (localStorage.getItem('color-theme') === 'light') {
+                    document.documentElement.classList.add('dark')
+                    localStorage.setItem('color-theme', 'dark')
+                } else {
+                    document.documentElement.classList.remove('dark')
+                    localStorage.setItem('color-theme', 'light')
+                }
+
+                // if NOT set via local storage previously
+            } else {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark')
+                    localStorage.setItem('color-theme', 'light')
+                } else {
+                    document.documentElement.classList.add('dark')
+                    localStorage.setItem('color-theme', 'dark')
+                }
+            }
+        })
     }, [])
 
     function switchToTeam(e: React.FormEvent, team: Team) {
@@ -65,7 +106,7 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                     {/* <!-- Primary Navigation Menu --> */}
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         <div className="flex justify-between h-16">
-                            <div className="flex">
+                            <div className="flex grow">
                                 {/* <!-- Logo --> */}
                                 <div className="flex-shrink-0 flex items-center">
                                     <Link href={route('home')}>
@@ -87,7 +128,7 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                                 </div>
 
                                 {!page.props.auth.user ? (
-                                    <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                                    <div className="sm:absolute sm:top-0 sm:right-0 p-6 text-right">
                                         <Link
                                             href={route('login')}
                                             className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
@@ -105,8 +146,41 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                                 ) : null}
                             </div>
 
+                            <div className="flex items-center">
+                                <div className="ml-3 relative">
+                                    <button
+                                        id="theme-toggle"
+                                        type="button"
+                                        className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+                                    >
+                                        <svg
+                                            id="theme-toggle-dark-icon"
+                                            className="w-5 h-5 hidden"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                                        </svg>
+                                        <svg
+                                            id="theme-toggle-light-icon"
+                                            className="w-5 h-5 hidden"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                                fillRule="evenodd"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+
                             {page.props.auth.user ? (
-                                <div className="sm:flex sm:items-center sm:ml-6">
+                                <div className="hidden md:visible sm:flex sm:items-center">
                                     <div className="ml-3 relative">
                                         {/* <!-- Teams Dropdown --> */}
                                         {page.props.jetstream.hasTeamFeatures ? (
