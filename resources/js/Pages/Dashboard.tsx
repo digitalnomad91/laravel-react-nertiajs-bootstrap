79b6e50307049d463f3a4ba111678464e05076ee
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-30 14:59:27 -0500

    Updated breadcrumb title.

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -70,7 +70,7 @@ export default function Dashboard() {
     return (
         <AppLayout
             title="Code Snippets"
-            renderHeader={() => <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
+            renderHeader={() => <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Code Snippets</h2>}
         >
             <div className="py-4">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
