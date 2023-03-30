7596b25583640e8cb70826d02b0be16d0dc307e0
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-03-30 14:33:18 -0500

    New routes.

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::controller(SnippetsController::class)->group(function () {
     Route::get('/snippet/{slug}', 'view')->name('snippet.view');
 });
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::get('/dashboard', 'App\Http\Controllers\SnippetsController@index')->name('dashboard');
-});
+//Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+//    Route::get('/dashboard', 'App\Http\Controllers\SnippetsController@index')->name('dashboard');
+//});
 
 Route::get('/payment', function () {
     return Inertia::render('Payment');
@@ -50,4 +50,8 @@ Route::post('/settings', [TwoFactorAuthController::class, 'confirm'])->name('two
 
 Route::get('/admin/login', function () {
     return redirect('/login');
+});
+
+Route::get('/dashboard', function () {
+    return redirect('/');
 });
