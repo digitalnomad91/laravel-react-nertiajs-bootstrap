ee8b1a03dc3eff00ad57992f7fdd24dc45d93d73
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-04-15 18:54:01 -0500

    Fixed navlink active status & removed bold.

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -97,7 +97,7 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
     }
 
     return (
-        <div>
+        <>
             <Head title={title} />
 
             <Banner />
@@ -110,20 +110,20 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                             <div className="flex grow">
                                 {/* <!-- Logo --> */}
                                 <div className="flex-shrink-0 flex items-center">
-                                    <Link href={route('home')}>
+                                    <Link href={route('snippet.home')}>
                                         <ApplicationMark className="block h-9 w-auto" />
                                     </Link>
                                 </div>
 
                                 {/* <!-- Navigation Links --> */}
                                 <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                    <NavLink href={route('home')} active={route().current('home')}>
+                                    <NavLink href={route('snippet.home')} active={route().current()?.includes('snippet.')}>
                                         Code Snippets
                                     </NavLink>
                                     <NavLink href="/premium" active={route().current('premium')}>
                                         Premium Members
                                     </NavLink>
-                                    <NavLink href="/blog" active={route().current('blog.home')}>
+                                    <NavLink href="/blog" active={route().current()?.includes('blog.')}>
                                         Blog
                                     </NavLink>
 
@@ -173,14 +173,14 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                                     <div className="ml-3 relative">
                                         <Link
                                             href={route('login')}
-                                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                            className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                                         >
                                             Log in
                                         </Link>
 
                                         <Link
                                             href={route('register')}
-                                            className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                            className="ml-4 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                                         >
                                             Register
                                         </Link>
@@ -200,7 +200,7 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                                                     <span className="inline-flex rounded-md">
                                                         <button
                                                             type="button"
-                                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150"
+                                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150"
                                                         >
                                                             {page.props.auth.user?.current_team?.name}
 
@@ -287,7 +287,7 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                                                     <span className="inline-flex rounded-md">
                                                         <button
                                                             type="button"
-                                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150"
+                                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150"
                                                         >
                                                             {page.props.auth.user?.name}
 
@@ -482,6 +482,94 @@ export default function AppLayout({ title, renderHeader, children, canLogin, can
                 {/* <!-- Page Content --> */}
                 <main>{children}</main>
             </div>
-        </div>
+
+            <footer className="relative dark:bg-gray-800 pt-8 pb-6">
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-wrap text-left lg:text-left">
+                        <div className="w-full lg:w-6/12 px-4">
+                            <h4 className="text-3xl font-semibold text-gray-500 dark:text-gray-400">Let's keep in touch!</h4>
+                            <h5 className="text-lg mt-0 mb-2 text-gray-500 dark:text-gray-400">
+                                Find us on any of these platforms, we respond 1-2 business days.
+                            </h5>
+                            <div className="mt-6 lg:mb-0 mb-6">
+                                <button className="bg-blue-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                                    <svg className="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+                                    </svg>
+                                </button>
+
+                                <button className="ml-4 bg-blue-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                                    <svg className="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+                                    </svg>
+                                </button>
+
+                                <button className="ml-4 bg-red-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                                    <svg className="w-5 h-5 fill-current" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0zm5.01 4.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 4.028 12c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM9.25 12C8.561 12 8 12.562 8 13.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.248.688 0 1.249-.561 1.249-1.249 0-.687-.562-1.249-1.25-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="w-full lg:w-6/12 px-4">
+                            <div className="flex flex-wrap items-top mb-6">
+                                <div className="w-full lg:w-4/12 px-4 ml-auto">
+                                    <span className="block uppercase text-gray-500 dark:text-gray-400 text-sm font-semibold mb-2">
+                                        Useful Links
+                                    </span>
+                                    <ul className="list-unstyled">
+                                        <li>
+                                            <Link className="text-gray-500 dark:text-gray-400 block pb-2 text-sm" href="/about">
+                                                About Us
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="text-gray-500 dark:text-gray-400 block pb-2 text-sm" href="/blog">
+                                                Blog
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="w-full lg:w-4/12 px-4">
+                                    <span className="block uppercase text-gray-500 dark:text-gray-400 text-sm font-semibold mb-2">
+                                        Other Resources
+                                    </span>
+                                    <ul className="list-unstyled">
+                                        <li>
+                                            <Link className="text-gray-500 dark:text-gray-400 block pb-2 text-sm" href="/terms-of-service">
+                                                Terms &amp; Conditions
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="text-gray-500 dark:text-gray-400 block pb-2 text-sm" href="/privacy-policy">
+                                                Privacy Policy
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="text-gray-500 dark:text-gray-400 block pb-2 text-sm" href="/contact">
+                                                Contact Us
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr className="my-6 border-blueGray-300" />
+                    <div className="flex flex-wrap items-center md:justify-between justify-center">
+                        <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+                            <div className="text-sm text-gray-500 dark:text-gray-400 font-semibold py-1">
+                                Copyright © <span id="get-current-year">2023</span>
+                                <a href="/" className="text-gray-500 dark:text-gray-400 hover:text-gray-800" target="_blank">
+                                    {' '}
+                                    IOPrompts
+                                </a>
+                                .
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
     )
 }
